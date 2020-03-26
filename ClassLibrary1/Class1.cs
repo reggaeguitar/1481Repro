052e79fe1e4fc7424e1d11a767c6a0a266d4ec85
Joe Brady <joseph.b.brady@gmail.com>
2020-03-26 14:23:41 -0600

    Initial commit

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,25 @@
+﻿using Azure.Storage.Blobs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public static void Main()
+        {
+            var conn = "replaceWithValidBlobConnectionString";
+            var client = GetBlobClient(conn);
+        }
+
+        private static BlobServiceClient GetBlobClient(string connectionString)
+        {
+            // this line throws the error!!!
+            BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+            return blobServiceClient;
+        }
+    }
+}
